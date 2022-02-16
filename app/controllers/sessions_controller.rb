52ce3ac173b98613c30a1911bf0ae655d05a15a2
SushmithaGovindarajuNuware <sushmitha.govindaraju@nuware.com>
2022-02-16 14:58:39 +0530

    authorization success

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
 		if user && user.authenticate(params[:session][:password])
 			# Log the user in and redirect to the user's show page.
 			log_in user
-			redirect_to user
-			flash.now[:success] = 'Login successful'
+			redirect_back_or user
+			# flash.now[:success] = 'Login successful'
 		else
 			# Create an error message.
 			flash.now[:danger] = 'Invalid email/password combination'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,10 +13,19 @@ class ActiveSupport::TestCase
 
    #include ApplicationHelper
 
-def is_logged_in?
+	def is_logged_in?
+	 !session[:user_id].nil?
+	end
 
- !session[:user_id].nil?
-
+	# Log in as a particular user.
+	def log_in_as(user)
+		session[:user_id] = user.id
+	end
 end
 
+class ActionDispatch::IntegrationTest
+	# Log in as a particular user.
+	def log_in_as(user, password: 'password', remember_me: '1')
+		post login_path, params: { session: { email: user.email, password: password, remember_me: remember_me } }
+	end
 end
